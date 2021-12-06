bd60a91982d2e63da58664bf123adbfd16662e79
Charles Sherk <cs897@cornell.edu>
2021-12-06 00:38:44 -0500

    Fixing Stefano's bug.

---
+++ day6/main.lisp
@@ -0,0 +1,43 @@
+(ql:quickload "cl-ppcre")
+
+(defun get-nums ()
+  (with-open-file (stream "input")
+    (let ((ln (read-line stream nil))
+	  (vec (make-array 0 :fill-pointer 0 :adjustable t)))
+      (mapcar (lambda (x) (vector-push-extend (parse-integer x) vec))
+	      (cl-ppcre:split "," ln))
+      vec)))
+
+;;stupid stupid stupid lol
+(defun step-1 (vec)
+  (let ((len (length vec)))
+    (loop :for i from 0 below len
+	  :do (if (= 0 (aref vec i))
+		  (progn (setf (aref vec i) 6)
+			 (vector-push-extend 8 vec))
+		  (setf (aref vec i) (1- (aref vec i)))))))
+
+(defun parta (vec)
+  (loop for i from 1 to 80
+	do (step-1 vec))
+  (length vec))
+
+(defun partb (vec)
+  (let ((arr (make-array 9 :initial-element 0 :adjustable t)))
+    (loop :for i across vec
+	  :do (setf (aref arr i) (1+ (aref arr i))))
+    (print arr)
+    (loop :for i from 1 to 256
+	  :do (let ((head (aref arr 0)))
+		(setq arr (make-array 8 :adjustable t :initial-contents
+			   (remove-if (lambda (x) t) arr :start 0 :end 1)))
+		(setf (aref arr 6) (+ (aref arr 6) head))
+	        (vector-push-extend head arr)))
+    (loop :for i across arr
+	  :sum i)))
+    (loop :for line = (let ((ln (read-line stream nil)))
+			(if ln (setq width (length ln)))
+			ln)
+	  :while line
+	  :collect (mapcar (lambda (p) (mapcar 'parse-integer (cl-ppcre:split "," p)))
+			   (cl-ppcre:split " -> " line)))))
